af2f416cfc5fcae2529d72fd36bf2ae19084b924
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-07-02 13:30:31 +0300

    SupervisorJob

---
+++ Coroutines/app/src/main/java/com/example/coroutines/SupervisorJob.kt
@@ -0,0 +1,77 @@
+package com.example.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.Main
+
+class SupervisorJob : AppCompatActivity() {
+
+    private val TAG: String = "AppDebug"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_supervisor_job)
+    }
+
+    private val handler = CoroutineExceptionHandler { _, exception ->
+        println("Exception thrown somewhere within parent or child: $exception.")
+    }
+
+    private val childExceptionHandler = CoroutineExceptionHandler{ _, exception ->
+        println("Exception thrown in one of the children: $exception.")
+    }
+
+    fun main(){
+
+        val parentJob = CoroutineScope(Main).launch(handler) {
+
+            supervisorScope { // *** Make sure to handle errors in children ***
+
+                // --------- JOB A ---------
+                val jobA =  launch {
+                    val resultA = getResult(1)
+                    println("resultA: $resultA")
+                }
+
+                // --------- JOB B ---------
+                val jobB = launch(childExceptionHandler) {
+                    val resultB = getResult(2)
+                    println("resultB: $resultB")
+                }
+
+                // --------- JOB C ---------
+                val jobC = launch {
+                    val resultC = getResult(3)
+                    println("resultC: $resultC")
+                }
+            }
+        }
+
+        parentJob.invokeOnCompletion { throwable ->
+            if(throwable != null){
+                println("Parent job failed: $throwable")
+            }
+            else{
+                println("Parent job SUCCESS")
+            }
+        }
+    }
+
+    private suspend fun getResult(number: Int): Int{
+        return withContext(Main){
+            delay(number*500L)
+            if(number == 2){
+                throw Exception("Error getting result for number: $number")
+            }
+            number*2
+        }
+    }
+
+
+    private fun println(message: String){
+        Log.d(TAG, message)
+    }
+
+}
